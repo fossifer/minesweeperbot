89af3b31df6dc2014422f4be42a18c21d47f81b6
fossifer <23732689+fossifer@users.noreply.github.com>
2023-06-02 15:03:20 -0500

    Fix reward when cardnum < 2

--- tgmsbot.py
+++ tgmsbot.py
@@ -348,14 +348,14 @@ def gen_reward(user, base, negative=True):
         else:
             return max(1, int(base * __floating(log(cardnum, 2))))
     def __get_cards(cardnum):
-        cards = base * __floating(1 / log(cardnum, 100))
         if cardnum >= 2:
+            cards = base * __floating(1 / log(cardnum, 100))
             if cards > 1.0:
                 return int(cards)
             else:
                 return int(__chance(cards))
         else:
-            return max(int(__floating(8.0)), cards)
+            return int(__floating(max(8.0, base)))
     # Negative rewards
     def restrict_mining(player):
         lost_cards = __lose_cards(player.immunity_cards)
